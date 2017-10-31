ff3ad3286e1f36b90fa8d0437ee727860b459340
Jasmine <93n.jas@gmail.com>
2017-10-31 16:24:54 -0400

    add a div so it shows both hello world and it is time for tea

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import React, {Component} from 'react';
 class Hello extends Component {
   render () {
     return (
-      <h1>Hello World!</h1>
+      <div>
+        <h1>Hello World!</h1>
+        <h3>It is time for tea</h3>
+      </div>  
     )
   }
 }
